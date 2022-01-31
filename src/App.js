d16cb689f139ac9ba039be5c52ef5172e9a8b47f
mubarek78 <shifamubarek@yahoo.com>
2022-01-30 20:54:17 -0700

    Project complited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './Banner';
+import Nav from './Nav';
+import requests from './requests';
+import Row from './Row';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+     <Nav />
+     <Banner />
+     <Row title="Netflix original" fetchUrl={requests.fetchNetflixOriginals} isLarge={true} />
+     <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+     <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies}/>
+     <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
+     <Row title="Comedy" fetchUrl={requests.fetchComedyMovies}/>
+     <Row title="Horor" fetchUrl={requests.fetchHororMovies}/>
+     <Row title="Romance" fetchUrl={requests.fetchRomanceMovies}/>
+     <Row title="Documentary" fetchUrl={requests.fetchDocumentaryMovies}/>
+  </>
 }
 
 export default App;
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "2a6bd7378f6da6d891f541bb444ae164";
+
+const requests = {
+fetchTrending: `/movie/popular?api_key=${API_KEY}&language=en-US&page=2`,
+fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_networks=213`,
+fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+fetchHororMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+fetchDocumentaryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;

aac7daef36b9f5a8aa1d2246cca6bec278703c57
mubarek78 <70493771+mubarek78@users.noreply.github.com>
2022-11-17 08:43:44 -0700

    updated

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import {PlayArrow} from "@material-ui/icons"
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import axios from "./axios";
 import requests from "./requests";
 import "./Banner.css";
-function Banner() {
+function Banner({type}) {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -21,6 +23,7 @@ function Banner() {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
   return (
+    
     <header
       className="banner"
       style={{
@@ -29,17 +32,44 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
+         {!type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
       <div className="banner__contents">
         <h1 className="banner__title">
           {movie?.title || movie?.name || movie.original_name}
         </h1>
-        <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
-        </div>
         <h1 className="banner__description">
           {truncate(movie?.overview, 150)}
         </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">
+          <PlayArrow />
+          <span>Play</span>
+          </button>
+          <button className="banner__button">
+          <InfoOutlinedIcon/>
+          <span>Info</span>
+          </button>
+        </div>
       </div>
       <div className="banner__fadeBottom" />
     </header>
--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from "react";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import "./Nav.css";
 
 
@@ -19,16 +20,36 @@ function Nav() {
 
   return (
     <div className={`nav ${show && "nav__black"}`}>
+    <div className="left">
       <img
-        className="nav__logo"
-        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/12c6f684-d447-4457-84fa-12033cfd581e/d9z3o32-2c299a83-49a9-44c2-bc23-b0b5070a546b.png/v1/fill/w_1024,h_512,strp/netflix_logo_background__2_by_sixmonthslate_d9z3o32-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD01MTIiLCJwYXRoIjoiXC9mXC8xMmM2ZjY4NC1kNDQ3LTQ0NTctODRmYS0xMjAzM2NmZDU4MWVcL2Q5ejNvMzItMmMyOTlhODMtNDlhOS00NGMyLWJjMjMtYjBiNTA3MGE1NDZiLnBuZyIsIndpZHRoIjoiPD0xMDI0In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.WKy-rj_8y7ziLuhhtAES7jddy-eUpz6PQQ331bp7rRI"
-        alt="Netflix Logo"
-      />
-      <img
-        className="nav__avatar"
-        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-        alt="Avatar logo"
-      />
+           className="nav__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+      <div className="left_menu">
+       <span>Homepage</span>
+       <span>Series</span>
+       <span>Movies</span>
+       <span>New and Popular</span>
+       <span>My List</span>
+       </div>
+      </div>
+      <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon dropdown" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
